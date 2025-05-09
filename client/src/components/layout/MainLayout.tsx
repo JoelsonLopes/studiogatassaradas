9ddee03d811aa3afd618301f40add8e0f1e18450
Joelson Lopes <joelsonlopes85@gmail.com>
2025-05-08 21:51:54 -0300

    refactor(layout): responsividade no layout principal e menu mobile

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,13 +1,17 @@
 import Sidebar from "./sidebar";
 import React from "react";
+import MobileNavigation from "./mobile-navigation";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1 bg-neutral-lightest">
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <div className="hidden md:flex">
+        <Sidebar />
+      </div>
+      <main className="flex-1 w-full bg-neutral-lightest pb-24 md:pb-0">
         {children}
       </main>
+      <MobileNavigation />
     </div>
   );
 } 
